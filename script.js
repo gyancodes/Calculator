4c43fc169d1b38cd59af40f532825c56587c043b
Gyan Prakash Tiwari <gyanprakasheng@gmail.com>
2023-04-27 14:43:47 +0530

    calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+// Variables
+let inputDisplay = document.querySelector('.inputDisplay');
+let ansDisplay = document.querySelector('.ansDisplay');
+let buttons = document.querySelectorAll('button');
+let eraseBtn = document.querySelector('#erase');
+let clearBtn = document.querySelector('#clear');
+let evaluate = document.querySelector('#evaluate');
+
+
